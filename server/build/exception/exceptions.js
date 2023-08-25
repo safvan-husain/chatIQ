e2c6c63db12a8648a761be0ca3d9ed036bc0ab14
safvan-husain <114668640+safvan-husain@users.noreply.github.com>
2023-08-25 17:15:49 +0530

    Delete server/build directory

--- server/build/exception/exceptions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DataBaseException = void 0;
-class Exception {
-}
-class DataBaseException extends Exception {
-}
-exports.DataBaseException = DataBaseException;
